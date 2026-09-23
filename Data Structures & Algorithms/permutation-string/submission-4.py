50d7dd1b4aff52a160400152d2e285000ff28d1b
Monko2k <65096789+Monko2k@users.noreply.github.com>
2026-09-22 22:16:49 -0400

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,55 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+
+        chars = defaultdict(int)
+        target = defaultdict(int)
+        matches = 0
+        for c1 in s1:
+            target[c1] += 1
+
+        for i in range(len(s1)):
+            c2 = s2[i]
+            if c2 in target:
+                if chars[c2] == target[c2]:
+                    matches -= 1
+                chars[c2] += 1
+                if chars[c2] == target[c2]:
+                    matches += 1
+            else:
+                chars[c2] += 1
+
+        if matches == len(target):
+            return True
+
+        l = 0
+        r = len(s1) - 1
+        while r < len(s2) - 1:
+            leftChar = s2[l]
+            l += 1
+            r += 1
+            newChar = s2[r]
+
+            if leftChar in target:
+                if chars[leftChar] == target[leftChar]:
+                    matches -= 1
+                chars[leftChar] -= 1
+                if chars[leftChar] == target[leftChar]:
+                    matches += 1
+            else:
+                chars[leftChar] -= 1
+
+            if newChar in target:
+                if chars[newChar] == target[newChar]:
+                    matches -= 1
+                chars[newChar] += 1
+                if chars[newChar] == target[newChar]:
+                    matches += 1
+            else:
+                chars[newChar] += 1
+
+            if matches == len(target):
+                return True
+
+        return False
